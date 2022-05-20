bd58db2ee14efcb3605a73b2f6202136e3bdc8c2
Kolodiazhnyi Serhii <elkrot@gmail.com>
2022-05-20 13:50:28 +0300

    Update cache.go

--- cache.go
+++ cache.go
@@ -36,7 +36,7 @@ func (c *Cache) Put(key, value string) {
 func (c *Cache) Keys() []string {
 	keys := []string{}
 	for k, v := range c.Values {
-		if v.deadline.IsZero() || v.deadline.Before(time.Now()) {
+		if v.deadline.IsZero() || v.deadline.After(time.Now()) {
 			keys = append(keys, k)
 		}
 	}
